0c82c172168f1a36f3708c896440d2ea448d0354
Oscar Wahltinez <oscar@wahltinez.org>
2020-06-08 07:38:57 -0400

    Mirror GHCN on gh-pages

--- README.md
+++ README.md
@@ -1,34 +1,11 @@
 # NOAA Weather Data
-This repo contains very simple code used to preprocess weather data from NOAA for it to be used in
-the [Open COVID-19 project](https://github.com/open-covid-19/data). This preprocessing performs the
-following steps:
-1. Downloads a list of all NOAA stations which provide at least max/min temperature measurements
-1. Downloads all daily data available since 2010 for max/min temperature and, if available, precipitation and snowfall measurements
-
-The output table is located at https://open-covid-19.github.io/weather/stations.csv and updated
-daily. It contains the following columns:
-
-| Name | Type | Description | Example |
-| ---- | ---- | ----------- | ------- |
-| **date** | `string` | ISO 8601 date (YYYY-MM-DD) of the datapoint | 2020-03-30 |
-| **station** | `string` | Identifier for the weather station | USC00206080 |
-| **latitude** | `double` | Floating point representing the geographic coordinate | 30.9756 |
-| **longitude** | `double` | Floating point representing the geographic coordinate | 112.2707 |
-| **elevation** | `integer` [meters] | Elevation above the sea level | 875 |
-| **minimum_temperature** | `double` [celsius] | Recorded hourly minimum temperature | 1.7 |
-| **maximum_temperature** | `double` [celsius] | Recorded hourly maximum temperature | 19.4 |
-| **rainfall** | `double` [millimeters] | Rainfall during the entire day | 51.0 |
-| **snowfall** | `double` [millimeters] | Snowfall during the entire day | 0.0 |
-
-## Sources of data
-All data in this repository is retrieved automatically. When possible, data is retrieved directly
-from the relevant authorities, like a country's ministry of health.
-
-| Data | Source | License |
-| ---- | ------ | ------- |
-| Weather | [NOAA](https://www.ncei.noaa.gov) | Custom (unrestricted for non-commercial use) |
+This repo contains very simple code used to mirror weather data from NOAA for it to be used in
+the [Open COVID-19 project](https://github.com/open-covid-19/data). It reproduces the last 2 years
+worth of records for all active stations from the [Global Historical Climatology Network][1].
 
 ## License
 The license of the data is subject to NOAA license terms (unrestricted for non-commercial use).
 Please see the [LICENSE](LICENSE.md) file for more information about the license for everything else
 in this repo.
+
+[1]: https://www.ncdc.noaa.gov/data-access/land-based-station-data/land-based-datasets/global-historical-climatology-network-ghcn
--- fetch.py
+++ fetch.py
@@ -1,122 +1,62 @@
 import sys
+from pathlib import Path
 from functools import partial
 from typing import Any, Optional, TextIO
-from multiprocessing import cpu_count
-from multiprocessing.pool import ThreadPool as Pool
 
+import requests
 from tqdm import tqdm
 from pandas import Series, read_csv
+from tqdm.contrib import concurrent
 
-OUTPUT_COLUMNS = [
-    "date",
-    "station",
-    "latitude",
-    "longitude",
-    "elevation",
-    "minimum_temperature",
-    "maximum_temperature",
-    "rainfall",
-    "snowfall",
-]
+KEEP_COUNT = 700
 
 
-def int_cast(value: Optional[Any]) -> Optional[int]:
+def process_ghcn(output_folder: Path, station: Series) -> None:
+
+    # Download the last KEEP_COUNT lines of the station's records
     try:
-        value = int(value)
-        return value
-    except:
-        return None
+        res = requests.get(
+            "https://www.ncei.noaa.gov/data"
+            f"/global-historical-climatology-network-daily/access/{station.id}.csv"
+        )
+        if res.status_code != 200:
+            raise RuntimeError(f"Unexpected status code {res.status_code}")
 
+        output = "\n".join(res.text.split("\n")[-KEEP_COUNT:])
+        with open(output_folder / f"{station.id}.csv", "w") as fd:
+            fd.write(output)
 
-def fix_temp(value: Optional[Any]) -> Optional[int]:
-    value = int_cast(value)
-    if value is not None:
-        return "{0:.1f}".format(value / 10.0)
-    else:
-        return None
-
-
-def process_station(fd: TextIO, record: Series) -> None:
-
-    # Read the records from the nearest station
-    station_url = (
-        "https://www.ncei.noaa.gov/data"
-        "/global-historical-climatology-network-daily/access/{}.csv"
-    ).format(record.station)
-    column_mapping = {
-        "DATE": "date",
-        "STATION": "station",
-        "LATITUDE": "latitude",
-        "LONGITUDE": "longitude",
-        "ELEVATION": "elevation",
-        "TMIN": "minimum_temperature",
-        "TMAX": "maximum_temperature",
-        "PRCP": "rainfall",
-        "SNOW": "snowfall",
-    }
-    data = read_csv(station_url, usecols=lambda column: column in column_mapping.keys())
-    data = data.rename(columns=column_mapping)
-
-    # Convert temperature to correct values
-    data["minimum_temperature"] = data["minimum_temperature"].apply(fix_temp)
-    data["maximum_temperature"] = data["maximum_temperature"].apply(fix_temp)
-
-    # Properly format coordinates and elevation
-    data["latitude"] = data["latitude"].apply(lambda x: "{0:.06g}".format(x))
-    data["longitude"] = data["longitude"].apply(lambda x: "{0:.06g}".format(x))
-    data["elevation"] = data["elevation"].apply(int_cast).astype("Int64")
-
-    # Get only data starting on 2015
-    data = data[data.date > "2014-12-31"]
-
-    # Make sure that all columns are in the record, even if null
-    for column in filter(lambda x: x not in data.columns, OUTPUT_COLUMNS):
-        data[column] = None
-
-    # Put the output columns in the appropriate order and get rid of empty records
-    data = data[OUTPUT_COLUMNS].dropna(subset=OUTPUT_COLUMNS[-4:], how="all")
-
-    # Write the processed station data
-    data.sort_values("date").to_csv(fd, header=False, index=False)
-    fd.flush()
+    except Exception as exc:
+        print(exc, file=sys.stderr)
 
 
 if __name__ == "__main__":
-
-    # Open the output file for writing and add header
-    output_path = sys.argv[1]
-    fd = open(output_path, "a")
-    fd.truncate(0)
-    fd.write(",".join(OUTPUT_COLUMNS) + "\n")
 
     # Get all the weather stations with data up until 2020
     stations_url = "https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-inventory.txt"
     stations = read_csv(
         stations_url,
         sep=r"\s+",
-        names=("station", "latitude", "longitude", "measurement", "year_start", "year_end"),
+        names=("id", "latitude", "longitude", "measurement", "year_start", "year_end"),
     )
-    stations = stations[stations.year_end == 2020][
-        ["station", "latitude", "longitude", "measurement"]
-    ]
+
+    # Filter only active stations
+    stations = stations[stations.year_end == 2020]
 
     # Filter stations that at least provide max and min temps
     measurements = ["TMIN", "TMAX"]
-    stations = stations.groupby(["station", "latitude", "longitude"]).agg(lambda x: "|".join(x))
+    stations = stations.groupby(["id", "latitude", "longitude"]).agg(lambda x: "|".join(x))
     stations = stations[
         stations.measurement.apply(lambda x: all(m in x.split("|") for m in measurements))
     ].reset_index()
 
-    # Make sure that the process function receives our file handle
-    map_func = partial(process_station, fd)
+    # Make sure that the process function receives output folder
+    output_folder = Path("output") / "ghcn"
+    map_func = partial(process_ghcn, output_folder)
+    output_folder.mkdir(parents=True, exist_ok=True)
 
     # We can skip the index when iterating over the records
     map_iter = (record for _, record in stations.iterrows())
 
     # Bottleneck is network so we can use lots of threads in parallel
-    records = list(
-        tqdm(Pool(cpu_count() * 4).imap_unordered(map_func, map_iter), total=len(stations),)
-    )
-
-    # Close the output file
-    fd.close()
+    concurrent.thread_map(map_func, map_iter, total=len(stations))
